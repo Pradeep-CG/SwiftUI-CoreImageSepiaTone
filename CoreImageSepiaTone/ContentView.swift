d73ab1e85983261e078cbb8720488c9bbe55f38c
iOS-Pradeep <pradeep.sagar.ios@gmail.com>
2020-07-02 23:13:12 +0530

    first commit

--- CoreImageSepiaTone/ContentView.swift
+++ CoreImageSepiaTone/ContentView.swift
@@ -7,10 +7,45 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
 
 struct ContentView: View {
+    
+    @State private var image: Image?
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            image?
+            .resizable()
+            .scaledToFit()
+        }
+    .onAppear(perform: loadImage)
+    }
+    func loadImage()  {
+        guard let inputImage = UIImage(named: "img") else {
+            return
+        }
+        let beginImage = CIImage(image: inputImage)
+        let context = CIContext()
+        let currentFilter = CIFilter.sepiaTone()
+        currentFilter.inputImage = beginImage
+        currentFilter.intensity = 1
+        
+        //get a CIImage
+        guard let outputImage = currentFilter.outputImage else {
+            return
+        }
+        // get CGImage
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            // convert that to a UIImage
+            let uiImage = UIImage(cgImage: cgimg)
+
+            // and convert that to a SwiftUI image
+            image = Image(uiImage: uiImage)
+        }
+        
     }
 }
 
